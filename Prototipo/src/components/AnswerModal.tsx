8d2e23c3d859ac40440044c543b428bc8d0b7785
Oridori... <124064876+oridori02@users.noreply.github.com>
2025-03-16 19:02:05 +0100

    Fix responsive Answermodal

--- Prototipo/src/components/AnswerModal.tsx
+++ Prototipo/src/components/AnswerModal.tsx
@@ -1,55 +1,57 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { Modal, View, Text, StyleSheet, Image } from 'react-native';
-import { globalStyles } from '../theme/theme';
+import {Modal, View, Text, StyleSheet, Image, Dimensions} from 'react-native';
+import {globalStyles} from '../theme/theme';
+
+const {height} = Dimensions.get('window');
 
 interface Props {
-    isVisible: boolean; // Controla si el modal está visible
-    message: string; // Mensaje a mostrar en el modal
-    image: any;
-    onClose: () => void; // Función para cerrar el modal
+  isVisible: boolean; // Controla si el modal está visible
+  message: string; // Mensaje a mostrar en el modal
+  image: any;
+  onClose: () => void; // Función para cerrar el modal
 }
 
-export const AnswerModal = ({ isVisible, message, image, onClose }: Props) => {
-    return (
-        <Modal
-            visible={isVisible}
-            animationType="fade"
-            transparent={true}
-            onRequestClose={onClose}
-        >
-            <View style={styles.modalOverlay}>
-                <View style={styles.modalContent}>
-                    <Image
-                        source={image}
-                        style={globalStyles.logo}
-                        resizeMode="contain"
-                    />
-                    <Text style={styles.modalText}>{message}</Text>
-                </View>
-            </View>
-        </Modal>
-    );
+export const AnswerModal = ({isVisible, message, image, onClose}: Props) => {
+  return (
+    <Modal
+      visible={isVisible}
+      animationType="fade"
+      transparent={true}
+      onRequestClose={onClose}>
+      <View style={styles.modalOverlay}>
+        <View style={styles.modalContent}>
+          <Image
+            source={image}
+            style={globalStyles.logo}
+            resizeMode="contain"
+          />
+          <Text style={styles.modalText}>{message}</Text>
+        </View>
+      </View>
+    </Modal>
+  );
 };
 
 const styles = StyleSheet.create({
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: 'transparent', // Fondo semitransparente
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        height: '60%',
-        width: '50%',
-        padding: 20,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalText: {
-        fontSize: 50,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'transparent', // Fondo semitransparente
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    height: '60%',
+    width: '50%',
+    padding: height * 0.0005,
+    backgroundColor: 'white',
+    borderRadius: height * 0.025,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: height * 0.06,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
