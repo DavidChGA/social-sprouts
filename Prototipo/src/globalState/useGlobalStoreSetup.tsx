1f6108b72bfbc1458f80aea347dbf331ddec7a6c
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2025-03-18 00:05:11 +0100

    Añadido al setup opciones para Emotions

--- Prototipo/src/globalState/useGlobalStoreSetup.tsx
+++ Prototipo/src/globalState/useGlobalStoreSetup.tsx
@@ -19,16 +19,25 @@ interface SequenceConfig extends Config {
   sequence: string;
 }
 
+interface EmotionsConfig extends Config {
+  emotion: string;
+  images: string;
+  rounds: string;
+}
+
 interface SetupState {
   session: Session;
   vocabularyConfigs: VocabularyConfig[];
   sequenceConfigs: SequenceConfig[];
+  emotionsConfigs: EmotionsConfig[];
 
   selectedVocabularyConfig: VocabularyConfig | null;
   selectedSequenceConfig: SequenceConfig | null;
+  selectedEmotionsConfig: EmotionsConfig | null;
 
   defaultVocabularyConfig: VocabularyConfig;
   defaultSequenceConfig: SequenceConfig;
+  defaultEmotionsConfig: EmotionsConfig;
 
   // Actions
   addVocabularyConfig: (config: VocabularyConfig) => void;
@@ -41,17 +50,28 @@ interface SetupState {
   updateSequenceConfig: (alias: string, config: Partial<SequenceConfig>) => void;
   removeSequenceConfig: (alias: string) => void;
   selectSequenceConfig: (alias: string) => void; //select
+
+  // Actions
+  addEmotionsConfig: (config: EmotionsConfig) => void;
+  updateEmotionsConfig: (alias: string, config: Partial<EmotionsConfig>) => void;
+  removeEmotionsConfig: (alias: string) => void;
+  selectEmotionsConfig: (alias: string) => void; //select
 }
 
 const useGlobalStoreSetup = create<SetupState>((set, get) => ({
-  session: {modules: []},
+  session: { modules: [] },
+
   vocabularyConfigs: [],
   sequenceConfigs: [],
+  emotionsConfigs: [],
+
   selectedVocabularyConfig: null,
   selectedSequenceConfig: null,
+  selectedEmotionsConfig: null,
 
   defaultVocabularyConfig: { alias: 'Predeterminado', category: 'Animal', images: '3', rounds: '3' },
   defaultSequenceConfig: { alias: 'Predeterminado', sequence: 'Lavado de manos' },
+  defaultEmotionsConfig: { alias: 'Predeterminado', emotion: 'Alegria', images: '', rounds: '' },
 
   // VOCABULARIO
   addVocabularyConfig: (config) =>
@@ -104,6 +124,33 @@ const useGlobalStoreSetup = create<SetupState>((set, get) => ({
         (config) => config.alias === alias
       ) || null,
     })),
+
+  //EMOCIONES
+  addEmotionsConfig: (config) =>
+    set((state) => ({
+      emotionsConfigs: [...state.emotionsConfigs, config],
+    })),
+  updateEmotionsConfig: (alias, newConfig) =>
+    set((state) => ({
+      emotionsConfigs: state.emotionsConfigs.map((config) =>
+        config.alias === alias ? { ...config, ...newConfig } : config
+      ),
+    })),
+  removeEmotionsConfig: (alias) =>
+    set((state) => ({
+      emotionsConfigs: state.emotionsConfigs.filter(
+        (config) => config.alias !== alias
+      ),
+      selectedEmotionsConfig:
+        state.selectedEmotionsConfig?.alias === alias ? null : state.selectedEmotionsConfig,
+    })),
+  selectEmotionsConfig: (alias) =>
+    set((state) => ({
+      selectedEmotionsConfig: state.emotionsConfigs.find(
+        (config) => config.alias === alias
+      ) || null,
+    })),
+
 }));
 
 export default useGlobalStoreSetup;
