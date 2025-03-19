62a2e956a843a4e6f19e053b59e53d6ea8771724
Oridori... <124064876+oridori02@users.noreply.github.com>
2025-03-19 12:00:53 +0100

    Añadidas img y formateado ficheros

--- Prototipo/src/assets/imageMapEmotions.ts
+++ Prototipo/src/assets/imageMapEmotions.ts
@@ -7,6 +7,48 @@ const imageMapEmotions: Record<string, any> = {
     'MIEDOPICTOGRAMA': require('../assets/img/emociones/pictogramas/miedo.png'),
     'SORPRESAPICTOGRAMA': require('../assets/img/emociones/pictogramas/sorpresa.png'),
     'TRISTEZAPICTOGRAMA': require('../assets/img/emociones/pictogramas/tristeza.png'),
+
+    //FELICIDAD
+    'FELICIDAD1': require('../assets/img/emociones/felicidad/felicidad1.png'),
+    'FELICIDAD2': require('../assets/img/emociones/felicidad/felicidad2.png'),
+    'FELICIDAD3': require('../assets/img/emociones/felicidad/felicidad3.png'),
+    'FELICIDAD4': require('../assets/img/emociones/felicidad/felicidad4.png'),
+    'FELICIDAD5': require('../assets/img/emociones/felicidad/felicidad5.png'),
+
+    //CONFUSION
+    'CONFUSION1': require('../assets/img/emociones/confusion/confusion1.png'),
+    'CONFUSION2': require('../assets/img/emociones/confusion/confusion2.png'),
+    'CONFUSION3': require('../assets/img/emociones/confusion/confusion3.png'),
+    'CONFUSION4': require('../assets/img/emociones/confusion/confusion4.png'),
+    'CONFUSION5': require('../assets/img/emociones/confusion/confusion5.png'),
+
+    //ENFADO
+    'ENFADO1': require('../assets/img/emociones/enfado/enfado1.png'),
+    'ENFADO2': require('../assets/img/emociones/enfado/enfado2.png'),
+    'ENFADO3': require('../assets/img/emociones/enfado/enfado3.png'),
+    'ENFADO4': require('../assets/img/emociones/enfado/enfado4.png'),
+    'ENFADO5': require('../assets/img/emociones/enfado/enfado5.png'),
+
+    //MIEDO
+    'MIEDO1': require('../assets/img/emociones/miedo/miedo1.png'),
+    'MIEDO2': require('../assets/img/emociones/miedo/miedo2.png'),
+    'MIEDO3': require('../assets/img/emociones/miedo/miedo3.png'),
+    'MIEDO4': require('../assets/img/emociones/miedo/miedo4.png'),
+    'MIEDO5': require('../assets/img/emociones/miedo/miedo5.png'),
+
+    //SORPRESA
+    'SORPRESA1': require('../assets/img/emociones/sorpresa/sorpresa1.png'),
+    'SORPRESA2': require('../assets/img/emociones/sorpresa/sorpresa2.png'),
+    'SORPRESA3': require('../assets/img/emociones/sorpresa/sorpresa3.png'),
+    'SORPRESA4': require('../assets/img/emociones/sorpresa/sorpresa4.png'),
+    'SORPRESA5': require('../assets/img/emociones/sorpresa/sorpresa5.png'),
+
+    //TRISTEZA
+    'TRISTEZA1': require('../assets/img/emociones/tristeza/tristeza1.png'),
+    'TRISTEZA2': require('../assets/img/emociones/tristeza/tristeza2.png'),
+    'TRISTEZA3': require('../assets/img/emociones/tristeza/tristeza3.png'),
+    'TRISTEZA4': require('../assets/img/emociones/tristeza/tristeza4.png'),
+    'TRISTEZA5': require('../assets/img/emociones/tristeza/tristeza5.png'),
   };
 
   export default imageMapEmotions;
