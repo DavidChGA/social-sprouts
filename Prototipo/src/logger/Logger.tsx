7d33f2b5ea9459c6d3c19cef6e6f4bef3ec292de
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2025-04-15 21:19:33 +0200

    Update Logger.tsx

--- Prototipo/src/logger/Logger.tsx
+++ Prototipo/src/logger/Logger.tsx
@@ -3,18 +3,18 @@ import { Log } from './LogInterface';
 
 class Logger {
   private static instance: Logger;
-  private logFilePath: string;
+  private logFilesPath: string;
 
   private constructor() {
-    this.changeFilePath();
+    this.changePath();
   }
 
-  private changeFilePath(): void {
-    const fecha = new Date();
-    const dia = String(fecha.getDate()).padStart(2, '0');
-    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
-    const anio = fecha.getFullYear();
-    this.logFilePath = `${RNFS.ExternalDirectoryPath}/${dia}-${mes}-${anio}.log`;
+  private changePath(): void {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    this.logFilesPath = `${RNFS.ExternalDirectoryPath}/${day}-${month}-${year}.log`;
   }
 
   static getInstance(): Logger {
@@ -24,22 +24,22 @@ class Logger {
     return Logger.instance;
   }
 
-  async log(log: Log): Promise<void> {
-    this.changeFilePath(); //Compruebo que el archivo es el último día
+  async log(log: Log) {
+    this.changePath(); //Compruebo que el archivo es el último día
     const json = JSON.stringify(log, null, 2);
 
     try {
       console.log('[LOG]', json);
-      await RNFS.appendFile(this.logFilePath, json + '\n', 'utf8');
+      await RNFS.appendFile(this.logFilesPath, json + '\n', 'utf8');
     } catch (error) {
       console.error('Error guardando log:', error);
     }
   }
 
-  async readLog(): Promise<string> {
-    this.changeFilePath();
+  async readLog() {
+    this.changePath();
     try {
-      return await RNFS.readFile(this.logFilePath, 'utf8');
+      return await RNFS.readFile(this.logFilesPath, 'utf8');
     } catch (error) {
       console.error('Error leyendo log:', error);
       return '';
